b58b415aefe452cf2d2cb5c1a30f8a3ea42a1a49
Thientvse <thientvse@gmail.com>
2023-02-17 14:32:28 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,51 @@
+import './style.css';
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+    const response = await fetch('http://localhost:8080/dream', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        prompt: data.get('prompt'),
+      }),
+    });
+
+    showSpinner();
+
+
+    if (response.ok) {
+      // main code here
+      hideSpinner();
+      const { image } = await response.json();
+
+      const result = document.querySelector('#result');
+      result.innerHTML = `<img src="${image}" width="512" />`;
+    } else {
+      hideSpinner();
+      const err = await response.text();
+      alert(err);
+      console.error(err);
+    }
+
+    
+});
+
+
+function showSpinner() {
+  const button = document.querySelector('button');
+  button.disabled = true;
+  button.innerHTML = 'Dreaming... <span class="spinner">🧠</span>';
+}
+
+function hideSpinner() {
+  const button = document.querySelector('button');
+  button.disabled = false;
+  button.innerHTML = 'Dream';
+}
